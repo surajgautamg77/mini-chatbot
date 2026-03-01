0f32ce3caa528d460d4d0febb64601e9fbd512e4
surajgautamg77 <surajgautamg77@gmail.com>
2026-03-01 11:08:20 +0530

    done

--- server/app/main.py
+++ server/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title=settings.PROJECT_NAME)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
